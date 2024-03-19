aae15e183b3c5e80860d7093bd8ca59b97add7af
RL <lojekrafal27@gmail.com>
2024-03-19 15:50:20 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Integer> liczby = new ArrayList<Integer>();
+        for(int i = 0; i < 50; i++)
+        {
+            liczby.add(i);
+        }
+        ArrayList<Integer> wylosowaneLiczby = new ArrayList<Integer>();
+        for(int i = 0; i < 6; i++)
+        {
+            //wylosowaneLiczby.add((int)Math.random() * liczby.size());
+        }
         ArrayList<Integer> podaneLiczby = new ArrayList<Integer>();
         Scanner scanner = new Scanner(System.in);
         while(podaneLiczby.size() < 6)
         {
             System.out.println("Podaj kolejną typowaną liczbę z zakresu <1,49>: ");
             Integer liczba = scanner.nextInt();
-            podaneLiczby.add(liczba);
+            if(podaneLiczby.contains(liczba))
+            {
+                System.out.println("Wytypowałeś już tą liczbę, podaj inną");
+            }
+            else if(liczba < 1 || liczba > 49)
+            {
+                System.out.println("Liczba z poza zakresu, wybierz inną");
+            }
+            else
+            {
+                podaneLiczby.add(liczba);
+            }
         }
         System.out.println(podaneLiczby);
     }
